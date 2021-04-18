4bc5f31b84937fa54e43b9c044983a891251c9c7
bbigal90 <bbigal90@gmail.com>
2021-04-18 17:17:34 +0900

    21.04.08
    오류 뜨지 않도록 수정

--- Semi_MSGR/src/msgr/client/MessengerClientThread.java
+++ Semi_MSGR/src/msgr/client/MessengerClientThread.java
@@ -1,56 +1,44 @@
 package msgr.client;
 
-import java.awt.Color;
-import java.awt.event.AdjustmentEvent;
-import java.awt.event.AdjustmentListener;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
-import java.util.Vector;
 
-import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
-import javax.swing.JScrollBar;
-import javax.swing.text.MutableAttributeSet;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
+
+import msgr.server.Protocol;
 
 public class MessengerClientThread extends Thread {
-	MessengerClientThread msgrClientThread = null;
-	String path = "C:\\Java\\dev_javaB\\dev_java\\src\\image\\";
-	String g_roomTitle = null;
+	MessengerClientView 	msgrClientView 	= null;
+	Socket 					Server 			= null;
+	ObjectOutputStream 		oos 			= null;
+	ObjectInputStream 		ois				= null;
 	
-	public MessengerClientThread(MessengerClient msgrClient) {
-		this.msgrClientThread = msgrClientThread;
-	}
-	public void hi () {
-		System.out.println( "bye 난 퇴근 해야할듯 .............");
-	}
-	public SimpleAttributeSet makeAttribute(String fcolor) {
-		SimpleAttributeSet sas = new SimpleAttributeSet();
-		sas.addAttribute(StyleConstants.ColorConstants.Foreground, new Color(Integer.parseInt(fcolor)));
-		return sas;
+	public MessengerClientThread(MessengerClientView msgrClientView) {
+		this.msgrClientView = msgrClientView;
 	}
 
 	public void run() {
-		String	msg		= null;
-		boolean	isStop	= false;
+		String msg = null;
+		boolean isStop = false;
 
 		while (!isStop) {// 무한루프 방지코드를 꼭 추가하자 - 변수처리하자, 조건식을 활용하자
-
 			try {
 				// 100|나초보
-				msg = (String) msgrClientThread.ois.readObject();
-				JOptionPane.showMessageDialog(msgrClientThread, msg + "\n");
+				msg = (String) msgrClientView.ois.readObject();
+				JOptionPane.showMessageDialog(msgrClientView, msg + "\n");
 				StringTokenizer	st			= null;
 				int				protocol	= 0;
 
 				if (msg != null) {
-					st = new StringTokenizer(msg, Protocol.seperator);
+					st = new StringTokenizer(
+							msg, Protocol.SEPERATOR);
 					protocol = Integer.parseInt(st.nextToken());
 				}
-
-				// JOptionPane.showMessageDialog(msgrClientThread, "프로토콜:"+protocol);
+				// JOptionPane.showMessageDialog(msgrClientView, "프로토콜:"+protocol);
 				switch (protocol) {
 				case Protocol.LOGIN: {}
 					break;
@@ -60,7 +48,7 @@ public class MessengerClientThread extends Thread {
 					break;
 				case Protocol.MEM_DELETE: {}	
 					break;
-				case Protocol.ROOM_CREATE_FRIEND: {}
+				case Protocol.ROOM_CREATE_BUDDY: {}
 					break;
 				case Protocol.ROOM_CREATE_OPENTALK: {}
 					break;
@@ -74,13 +62,11 @@ public class MessengerClientThread extends Thread {
 					break;
 				case Protocol.ROOM_DELETE: {}
 					break;
-				case Protocol.MESSAGE: {}
+				case Protocol.BUDDY_ADD: {}
 					break;
-				case Protocol.FRIEND_ADD: {}
+				case Protocol.BUDDY_LIST: {}
 					break;
-				case Protocol.FRIEND_LIST: {}
-					break;
-				case Protocol.FRIEND_DELETE: {}
+				case Protocol.BUDDY_DELETE: {}
 					break;
 				case Protocol.MESSAGE: {}
 					break;
@@ -88,12 +74,14 @@ public class MessengerClientThread extends Thread {
 					break;
 				case Protocol.ATTACHMENT: {}
 					break;
-				case Protocol.FLAG_OPENROOM: {}
+				case Protocol.FLAG_OPEN_ROOM: {}
 					break;
-				case Protocol.FLAG_FRIENDROOM: {}
-				break;
-			catch (Exception e) {
+				case Protocol.FLAG_BUDDY_ROOM: {}
+					break;
+				}
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			}
 		}
+	}
+}
