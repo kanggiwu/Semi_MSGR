e3058e2fb391576f3985a76a915b048da85adc1d
kanggiwu <wu10604@gmail.com>
2021-04-18 17:13:03 +0900

    [210418 17:12 강지우]
    서버 로그인, 로그아웃, 회원탈퇴

--- Semi_MSGR/src/msgr/server/MessengerTalkRoom.java
+++ Semi_MSGR/src/msgr/server/MessengerTalkRoom.java
@@ -1,5 +1,23 @@
 package msgr.server;
 
-public class MessengerTalkRoom {
+import java.util.List;
 
+public class MessengerTalkRoom {
+	private List<MessengerServerThread> roomInList = null;
+	private int talk_no = -1;
+	private int is_private = -1;
+	private List<String> idList = null;
+	private List<String> nicknameList = null;
+	private int currentClientNum = -1;
+	private String talkTitle = null;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
